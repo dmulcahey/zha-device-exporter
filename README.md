e5808f1b7bc61cfada8c3676c3b7d775fefbfee5
David Mulcahey <david.mulcahey@me.com>
2024-08-25 10:25:35 -0400

    update to dump diagnostic files

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Custom HA component to export ZHA Zigbee devices
 
 # Usage
 
-Run service `zha_device_exporter.export_devices` This will export a JSON file (`zha-devices.json`) to the directory `zha-device-export` in the Home Assistant configuration directory.
+Run service `zha_device_exporter.export_devices` This will export device diagnostic JSON files to the directory `zha-device-export` in the Home Assistant configuration directory.
--- custom_components/zha_device_exporter/__init__.py
+++ custom_components/zha_device_exporter/__init__.py
@@ -1,16 +1,30 @@
 """Custom component used to export ZHA Zigbee devices to a JSON file."""
+
 import logging
 import os
+from typing import Any
 
-from homeassistant.util.json import save_json
 import voluptuous as vol
-
+from homeassistant.components.zha.diagnostics import get_endpoint_cluster_attr_data
+from homeassistant.components.zha.helpers import (
+    ZHADeviceProxy,
+    ZHAGatewayProxy,
+    async_get_zha_device_proxy,
+    get_zha_gateway_proxy,
+)
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.json import save_json
+from slugify import slugify
 
 ATTR_OUTPUT_DIR = "output_dir"
 DOMAIN = "zha_device_exporter"
 CONFIG_OUTPUT_DIR_NAME = "zha-device-export"
 SERVICE_EXPORT_DEVICES = "export_devices"
 SERVICE_SCHEMAS = {SERVICE_EXPORT_DEVICES: vol.Schema({})}
+ATTRIBUTES = "attributes"
+CLUSTER_DETAILS = "cluster_details"
+UNSUPPORTED_ATTRIBUTES = "unsupported_attributes"
 
 LOGGER = logging.getLogger(__name__)
 
@@ -19,11 +33,6 @@ async def async_setup(hass, config):
     """Set up zha-device-exporter from config."""
     if DOMAIN not in config:
         return True
-
-    try:
-        zha_gateway = hass.data["zha"]["zha_gateway"]
-    except KeyError:
-        return False
 
     output_dir = os.path.join(hass.config.config_dir, CONFIG_OUTPUT_DIR_NAME)
 
@@ -39,14 +48,24 @@ async def async_setup(hass, config):
         if not await hass.async_add_executor_job(mkdir, output_dir):
             return False
 
-    def get_devices():
-        """Get all ZHA Zigbee device information."""
-        return [device.async_get_info() for device in zha_gateway.devices.values()]
-
-    async def export_devices_handler(service):
-        """Export ZHA devices to a json file right now."""
-        file_name = os.path.join(output_dir, "zha-devices.json")
-        await hass.async_add_executor_job(save_json, file_name, get_devices())
+    async def export_devices_handler(service) -> None:
+        """Export ZHA device diagnostics files."""
+        gateway_proxy: ZHAGatewayProxy = get_zha_gateway_proxy(hass)
+        for device in gateway_proxy.device_proxies.values():
+            zha_device_proxy: ZHADeviceProxy = async_get_zha_device_proxy(
+                hass, device.device_id
+            )
+            device_info: dict[str, Any] = zha_device_proxy.zha_device_info
+            device_info[CLUSTER_DETAILS] = get_endpoint_cluster_attr_data(
+                zha_device_proxy.device
+            )
+            file_name = os.path.join(
+                output_dir,
+                slugify(
+                    f"{zha_device_proxy.device.manufacturer}-{zha_device_proxy.device.model}.json"
+                ),
+            )
+            await hass.async_add_executor_job(save_json, file_name, device_info)
 
     hass.services.async_register(
         DOMAIN,
@@ -54,5 +73,20 @@ async def async_setup(hass, config):
         export_devices_handler,
         schema=SERVICE_SCHEMAS[SERVICE_EXPORT_DEVICES],
     )
+
+    return True
+
+
+async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
+    """Set up ZHA.
+
+    Will automatically load components to support devices found on the network.
+    """
+
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
+    """Unload ZHA config entry."""
 
     return True
